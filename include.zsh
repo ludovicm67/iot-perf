1ae003dff9e45534908ae97b2a94497085957f07
Quentin Gliech <quentingliech@gmail.com>
2020-01-02 14:46:00 +0100

    gracefully stop experiment & log timestamps

--- include.zsh
+++ include.zsh
@@ -1,7 +1,7 @@
 . ./config.zsh
 
 setup_tunnel () {
-  while (iotlab experiment wait -i $1) {
+  while (iotlab experiment wait -i $1 2>&1 > /dev/null) {
     ssh_iotlab -- sudo tunslip6.py -v3 -L -a m3-$gateway -p 20000 $prefix
   }
 }
@@ -23,4 +23,24 @@ experiment_string () {
 node_ip () {
   local uid=$uid_map[$1]
   echo "${prefix%/*}$uid"
+}
+
+add_timestamp () {
+  while IFS= read -r line; do
+    printf "%s\t%s\n" "$(date '+%s')" "$line"
+  done
+}
+
+local stopping=false
+
+stop () {
+  echo
+  if [ $stopping = false ]; then
+    echo "Stopping experiment…" >&2
+    iotlab experiment stop -i $1 > /dev/null
+    stopping=true
+  else
+    echo "Exiting." >&2
+    exit
+  fi
 }
--- run.zsh
+++ run.zsh
@@ -3,18 +3,29 @@
 set -euo pipefail
 . ./include.zsh
 
+echo "Submitting experiment" >&2
+
 iotlab experiment submit -d 5 \
   -l $(experiment_string nodes $firmware_nd) \
   -l $(experiment_string gateway $firmware_gw) \
   | python3 -c "import sys, json; print(json.load(sys.stdin)['id'])" \
   | read experiment_id
 
-iotlab experiment wait -i $experiment_id
+trap "stop $experiment_id" INT HUP TERM
+
+iotlab experiment wait -i $experiment_id > /dev/null
 
 local -A uid_map=($(iotlab experiment get -r -i $experiment_id | python3 uid_map.py))
 
-setup_tunnel $experiment_id > tunnel-stdout 2> tunnel-stderr &
+setup_tunnel $experiment_id | add_timestamp > m3-$gateway &
+echo "border-router m3-$gateway" $(node_ip m3-$gateway)
+
 for node ($nodes) {
-  node_ip m3-$node
+  echo "server m3-$node" $(node_ip m3-$node)
+  ssh_iotlab -- nc m3-$node 20000 | add_timestamp > m3-$node &
 }
-ssh_iotlab -- serial_aggregator -l $(experiment_string nodes) | tee aggregator-out
+
+wait
+
+echo
+echo "End."
