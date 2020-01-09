67bffffefb3c9eccebda809c33bdf416a3bacd7c
Quentin Gliech <quentingliech@gmail.com>
2020-01-10 00:33:22 +0100

    forgot to add firmware files

---
+++ firmwares/nullrdc-full/firmwares.zsh
@@ -0,0 +1,2 @@
+local firmware_gw=$contiki/examples/ipv6/rpl-border-router/border-router.iotlab-m3
+local firmware_nd=$contiki/examples/iotlab/04-er-rest-example/er-example-server.iotlab-m3
---
+++ firmwares/tsch-full/firmwares.zsh
@@ -0,0 +1,2 @@
+local firmware_gw=$contiki/examples/iotlab/05-rpl-tsch-border-router/border-router.iotlab-m3
+local firmware_nd=$contiki/examples/iotlab/06-rpl-tsch-coap/er-example-server.iotlab-m3
